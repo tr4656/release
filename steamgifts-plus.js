eaafb7dab28b9f2795f465b2011822e7098766c7
tr4656 <yifanj1@yahoo.com>
2013-12-01 22:04:25 -0800

    Update steamgifts-plus.js

--- steamgifts-plus.js
+++ steamgifts-plus.js
@@ -2293,7 +2293,7 @@
 		
 			var scriptAwards = document.createElement('script');
 			scriptAwards.type = 'text/javascript';
-			scriptAwards.src = 'http://github.com/sgplus-alternative/release/raw/master/awards.js';
+			scriptAwards.src = 'http://github.com/tr4656/release/raw/master/awards.js';
 			document.head.appendChild(scriptAwards);
 		}
 	}
